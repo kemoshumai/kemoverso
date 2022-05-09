5750346023fd36667d6498bf65e885332d662482
kemoshumai <87066961+kemoshumai@users.noreply.github.com>
2022-05-09 16:51:01 +0900

    Add a method of loading a scene from code.

--- src/kemoverso.ts
+++ src/kemoverso.ts
@@ -4,17 +4,23 @@ import KemoversoEngine from "./KemoversoEngine";
 
 export default class Kemoverso
 {
-    public establishWithCanvas(canvasElement:HTMLElement, sceneFilepath: string)
-    {
-        this.establish(canvasElement as HTMLCanvasElement, sceneFilepath);
-    }
-    public async establish(canvasElement:HTMLCanvasElement, sceneFilepath: string): Promise<void>
+    public async establishWithCanvasFromFile(canvasElement:HTMLElement, sceneFilepath: string)
     {
         const sceneFolderpath:string = sceneFilepath.split("/").reverse().slice(1).reverse().join("/")+"/" ?? "";
         const sceneFilename:string = sceneFilepath.split('/')?.pop()?.split('\\')?.pop() ?? "";
         console.log("Loading...:",sceneFolderpath,sceneFilename);
+        const scene:BABYLON.Scene = await BABYLON.SceneLoader.LoadAsync(sceneFolderpath,sceneFilename);
+        this.establish(canvasElement as HTMLCanvasElement, scene);
+    }
+    public establishWithCanvas(canvasElement:HTMLElement, scene: BABYLON.Scene)
+    {
+        this.establish(canvasElement as HTMLCanvasElement, scene);
+    }
+    public async establish(canvasElement:HTMLCanvasElement, scene: BABYLON.Scene): Promise<void>
+    {
+        
         const kemoversoEngine:KemoversoEngine = new KemoversoEngine(new BABYLON.Engine(canvasElement,true),canvasElement);
-        await kemoversoEngine.createScene(await BABYLON.SceneLoader.LoadAsync(sceneFolderpath,sceneFilename, kemoversoEngine.babylonEngine));
+        await kemoversoEngine.createScene(scene);
         kemoversoEngine.render();
     }
 }
